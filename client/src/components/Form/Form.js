32325b7d8ac7778ec28c998999cc0c4bed2ba5cc
Patrick <78768245+patrick-cha@users.noreply.github.com>
2022-05-23 01:07:44 -0700

    added recommended feature

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { useHistory } from 'react-router-dom';
 
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
@@ -11,9 +11,10 @@ import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({ title: '', message: '', tags: '', selectedFile: '' })
-    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
+    const post = useSelector((state) => currentId ? state.posts.posts.find((p) => p._id === currentId) : null)
     const classes = useStyles();
     const dispatch = useDispatch();
+    const history = useHistory();
     const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
@@ -31,7 +32,7 @@ const Form = ({ currentId, setCurrentId }) => {
         if(currentId) {
             dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }))
         } else {
-            dispatch(createPost({ ...postData, name: user?.result?.name }))
+            dispatch(createPost({ ...postData, name: user?.result?.name }, history))
         }
         clear();
     }
